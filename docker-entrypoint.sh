f74603333b332b77db63b2a71f72a5a98ea47e42
Eric Tendian <erictendian@gmail.com>
2024-09-15 19:34:58 -0500

    Make entrypoint script more generic

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,20 +6,6 @@ if [ -x "${1}" ]; then
     exec "$@"
 fi
 
-add_to_haproxy() {
-    if [ -n "${API_KEY}" ] && [ -n "${API_BASE_URL}" ] && [ -n "${VAST_CONTAINERLABEL}" ] && [ -n "${VAST_TCP_PORT_9000}" ] && [ -n "${PUBLIC_IPADDR}" ]; then
-        echo "Adding service to load balancer" | tee -a /onstart.log
-        curl -fSs -H "Authorization: Bearer ${API_KEY}" "${API_BASE_URL}/haproxy" -d name=${VAST_CONTAINERLABEL} -d server=${PUBLIC_IPADDR} -d port=${VAST_TCP_PORT_9000} --connect-timeout 5 2>&1 | tee -a /onstart.log
-    fi
-}
-
-remove_from_haproxy() {
-    if [ -n "${API_KEY}" ] && [ -n "${API_BASE_URL}" ] && [ -n "${VAST_CONTAINERLABEL}" ] && [ -n "${VAST_TCP_PORT_9000}" ] && [ -n "${PUBLIC_IPADDR}" ]; then
-        echo "Removing service from load balancer" | tee -a /onstart.log
-        curl -fSs -H "Authorization: Bearer ${API_KEY}" -X DELETE "${API_BASE_URL}/haproxy" -d name=${VAST_CONTAINERLABEL} --connect-timeout 5 2>&1 | tee -a /onstart.log
-    fi
-}
-
 export ASR_MODEL_PATH="$(realpath ~/.cache/whisper)"
 cd /app
 # if not a directory or directory is empty, download the model
@@ -29,14 +15,32 @@ then
     $POETRY_VENV/bin/python3 app/download_model.py 2>&1 | tee -a /onstart.log
 fi
 
-set -m
-gunicorn --bind 0.0.0.0:9000 --workers ${ASR_WORKERS:-1} --timeout 0 app.webservice:app -k uvicorn.workers.UvicornWorker $@ &
+if [ -n "${WEBHOOK_SECRET}" ] && [ -n "${WEBHOOK_URL}" ]; then
+    SERVER_NAME=${SERVER_NAME:-${VAST_CONTAINERLABEL:-$(hostname)}}
+    SERVER_IP=${SERVER_IP:-${PUBLIC_IPADDR:-$(curl -s "https://ipinfo.io/ip")}}
+    SERVER_PORT=${SERVER_PORT:-${VAST_TCP_PORT_9000:-9000}}
 
-# Wait for the server to start
-sleep 5
+    set -m
+    gunicorn --bind 0.0.0.0:9000 --workers ${ASR_WORKERS:-1} --timeout 0 app.webservice:app -k uvicorn.workers.UvicornWorker $@ &
 
-add_to_haproxy
+    # Wait for the server to start
+    sleep 5
 
-trap 'remove_from_haproxy' SIGINT SIGTERM EXIT
+    add_to_haproxy() {
+        echo "Adding service to load balancer" | tee -a /onstart.log
+        curl -fSs -H "Authorization: Bearer ${WEBHOOK_SECRET}" "${WEBHOOK_URL}" -d name=${SERVER_NAME} -d server=${SERVER_IP} -d port=${SERVER_PORT} --connect-timeout 5 2>&1 | tee -a /onstart.log
+    }
 
-fg %1
+    remove_from_haproxy() {
+        echo "Removing service from load balancer" | tee -a /onstart.log
+        curl -fSs -H "Authorization: Bearer ${WEBHOOK_SECRET}" -X DELETE "${WEBHOOK_URL}" -d name=${SERVER_NAME} --connect-timeout 5 2>&1 | tee -a /onstart.log
+    }
+
+    add_to_haproxy
+
+    trap 'remove_from_haproxy' SIGINT SIGTERM EXIT
+
+    fg %1
+else
+    gunicorn --bind 0.0.0.0:9000 --workers ${ASR_WORKERS:-1} --timeout 0 app.webservice:app -k uvicorn.workers.UvicornWorker $@
+fi
